29902a4cc6d4da4fb0e278515a236edea79f2ee1
Bentley Hensel <bentley.hensel@civicactions.com>
2023-07-21 12:36:46 -0400

    Fixed Python Path

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 # app/main.py
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from analyze import get_variables
 
-
 url = 'https://www.digital.gov'
-
 
 variables = get_variables(url)
 
